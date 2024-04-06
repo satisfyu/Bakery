8897966d46f0457631b5dd1a8c76c289dd434a17
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-06 13:40:56 +0200

    Fix: RP for forge

--- common/src/main/java/satisfy/bakery/Bakery.java
+++ common/src/main/java/satisfy/bakery/Bakery.java
@@ -1,9 +1,11 @@
 package satisfy.bakery;
 
+import de.cristelknight.doapi.DoApiExpectPlatform;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import satisfy.bakery.event.CommonEvents;
 import satisfy.bakery.registry.*;
+import satisfy.bakery.util.BakeryIdentifier;
 
 public class Bakery {
     public static final String MOD_ID = "bakery";
@@ -20,6 +22,8 @@ public class Bakery {
         ScreenHandlerTypeRegistry.init();
         CommonEvents.init();
         TabRegistry.init();
+
+        DoApiExpectPlatform.registerBuiltInPack(Bakery.MOD_ID, new BakeryIdentifier("minecraft_bricks"), false);
     }
 
     public static void commonInit() {
