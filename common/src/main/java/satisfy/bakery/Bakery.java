adafcd9d426fe7ec215dafbe51e35e7c75966918
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-05-05 00:00:13 +0200

    Added: Bakery resource pack

--- common/src/main/java/satisfy/bakery/Bakery.java
+++ common/src/main/java/satisfy/bakery/Bakery.java
@@ -1,14 +1,15 @@
 package satisfy.bakery;
 
+import de.cristelknight.doapi.DoApiEP;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import satisfy.bakery.event.CommonEvents;
 import satisfy.bakery.registry.*;
+import satisfy.bakery.util.BakeryIdentifier;
 
 public class Bakery {
     public static final String MOD_ID = "bakery";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
 
     public static void init() {
         ObjectRegistry.init();
@@ -20,6 +21,7 @@ public class Bakery {
         CommonEvents.init();
         TabRegistry.init();
 
+        DoApiEP.registerBuiltInPack(Bakery.MOD_ID, new BakeryIdentifier("minecraft_bricks"), false);
     }
 
     public static void commonInit() {
--- common/src/main/java/satisfy/bakery/BakeryExpectPlatform.java
+++
@@ -1,4 +0,0 @@
-package satisfy.bakery;
-
-public class BakeryExpectPlatform {
-}
