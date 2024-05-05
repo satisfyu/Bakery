8a535df75aa9a90d9a6951f4e6bbe57d89687bce
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-05-05 22:22:51 +0200

    DoAPI final 1.2.10 update

--- common/src/main/java/satisfy/bakery/client/ClientStorageTypes.java
+++ common/src/main/java/satisfy/bakery/client/ClientStorageTypes.java
@@ -1,7 +1,7 @@
 package satisfy.bakery.client;
 
-import de.cristelknight.doapi.client.render.block.storage.StorageBlockEntityRenderer;
-import de.cristelknight.doapi.client.render.block.storage.StorageTypeRenderer;
+import de.cristelknight.doapi.client.render.block.storage.api.StorageBlockEntityRenderer;
+import de.cristelknight.doapi.client.render.block.storage.api.StorageTypeRenderer;
 import net.minecraft.resources.ResourceLocation;
 import satisfy.bakery.Bakery;
 import satisfy.bakery.client.render.*;
